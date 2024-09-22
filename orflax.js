7f2bea03ebe4e60ff382be538e1f879de4a08af7
Meenal <meenalgautam41@gmail.com>
2024-09-22 09:08:30 +0530

    new

---
+++ orflax.js
@@ -0,0 +1,9 @@
+// Function to open the contact form
+function openContactForm() {
+    document.getElementById('contactForm').style.display = 'block';
+}
+
+// Function to close the contact form
+function closeContactForm() {
+    document.getElementById('contactForm').style.display = 'none';
+}
